ae91a171ef58619921c78eaf9ffc53319c4fc56c
Wojciech Andrzejuk <wojciech.andrzejuk@wp.pl>
2024-04-26 21:30:54 +0200

    Update body of function

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 def palindrome(word):
-    pass
+  palindrome1 = ""
+  palindrome2 = ""
+  for i in range(len(word)):
+    palindrome1 += word[i]
+  for i in range(len(word)-1, -1, -1):
+    palindrome2 += word[i]
+  if palindrome1 == palindrome2:
+    return True
+  else:
+    return False
