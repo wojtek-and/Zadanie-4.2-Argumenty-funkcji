e38a77af722decea6b010664c03c5eef128ffd52
Wojciech Andrzejuk <wojciech.andrzejuk@wp.pl>
2024-04-26 21:33:49 +0200

    Add docstrings

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""
+The function determines whether the given word is a palindrome. It compares whether a given word when read from left to right sounds the same when read from right to left.
+At the end it returns True/False.
+"""
+
 def palindrome(word):
   palindrome1 = ""
   palindrome2 = ""
